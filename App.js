510842485cccb920dc5484ec265dad5564af15a9
Rio Sanka <threehouse.second@gmail.com>
2019-11-08 17:37:32 +0900

    FlatListの子をコンポーネント化

--- App.js
+++ App.js
@@ -1,55 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
+import { SafeAreaView, FlatList, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 
-const DATA = [
-  {
+import Item from './components/Item';
+
+export default class App extends React.Component {
+  state = {
+    itemData: [{
     id: '1',
     title: 'First Item',
-  },
-  {
-    id: '2',
-    title: 'Second Item',
-  },
-  {
-    id: '3',
-    title: 'Third Item'
-  },
-]
-
-// Itemコンポーネント
-function Item({title}) {
-  return(
-    <View style={styles.item}>
-      <Text>{title}</Text>
-    </View>
-  );
-}
-
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={({item}) => <Item title={item.title}/>}
-        keyExtractor={item => item.id}
-      />
-    </SafeAreaView>
-  );
+    },
+    {
+      id: '2',
+      title: 'Second Item',
+    },
+    {
+      id: '3',
+      title: 'Third Item'
+      },]
+  }
+  
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={this.state.itemData}
+          renderItem={({ item }) => <Item title={item.title + item.id} />}
+          keyExtractor={item => item.id}
+        />
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: Constants.statusBarHeight,
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
   },
 });
