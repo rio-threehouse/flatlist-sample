ecc7520ca353bb963df57b0e5c0718aef2e1f150
Rio Sanka <threehouse.second@gmail.com>
2019-11-12 13:37:49 +0900

    下部にButton(TouchableOpacity)を追加

--- App.js
+++ App.js
@@ -1,33 +1,40 @@
 import React from 'react';
-import { SafeAreaView, FlatList, StyleSheet } from 'react-native';
+import { SafeAreaView, FlatList, StyleSheet,TouchableOpacity, Text } from 'react-native';
 import Constants from 'expo-constants';
 
 import Item from './components/Item';
 
 export default class App extends React.Component {
   state = {
-    itemData: [{
-    id: '1',
-    title: 'First Item',
-    },
-    {
-      id: '2',
-      title: 'Second Item',
-    },
-    {
-      id: '3',
-      title: 'Third Item'
-      },]
+    itemData: [],
+    listUpdate: '1',
   }
-  
+
+  addItem = () => {
+    this.state.itemData.push({
+      id: this.state.listUpdate,
+      title: 'Item',
+    });
+
+    this.setState({
+      itemData: this.state.itemData,
+      listUpdate: String( Number( this.state.listUpdate ) + 1 )
+    });
+  }
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <FlatList
           data={this.state.itemData}
-          renderItem={({ item }) => <Item title={item.title + item.id} />}
+          renderItem={({ item }) => <Item title={item.title + '-' + item.id} />}
           keyExtractor={item => item.id}
+          extraData={this.state.listUpdate}
         />
+        <TouchableOpacity
+        onPress={this.addItem}>
+          <Text style={styles.addButton}>Add</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     );
   }
@@ -38,4 +45,10 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: Constants.statusBarHeight,
   },
+  addButton: {
+    textAlign: 'center',
+    backgroundColor: 'skyblue',
+    margin: 10,
+    padding: 10,
+  }
 });
